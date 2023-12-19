3e11c50766fddcc7abc2d225e6ffa5a41cf0f2c2
hamza-okutucu <hamza.okutucu@outlook.com>
2023-12-19 15:26:48 +0100

    update

--- src/main/java/melowave/MeloWaveApplication.java
+++ src/main/java/melowave/MeloWaveApplication.java
@@ -11,10 +11,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableOpenApi
 public class MeloWaveApplication {
 	
 		@Bean
