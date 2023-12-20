28759f14c6fcf5c14eb169c0366ccf17693f6d6b
hamza-okutucu <hamza.okutucu@outlook.com>
2023-12-20 22:26:31 +0100

    update

--- src/main/java/melowave/config/SecurityConfig.java
+++ src/main/java/melowave/config/SecurityConfig.java
@@ -36,21 +36,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests()
-            .antMatchers(
-                "/login",
-                "/user/create",
-                "/song/find/*",
-                "/song/search/count",
-                "/song/stream/*",
-                "/song/artists",
-                "/song/genres",
-                "/song/status",
-                "/v2/api-docs",
-                "/swagger-resources/**",
-                "/swagger-ui/**",
-                "/webjars/**" 
-            ).permitAll();
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers("/user/create").permitAll();
+        http.authorizeRequests().antMatchers("/song/find/*").permitAll();
+        http.authorizeRequests().antMatchers("/song/search/count").permitAll();
+        http.authorizeRequests().antMatchers("/song/stream/*").permitAll();
+        http.authorizeRequests().antMatchers("/song/artists").permitAll();
+        http.authorizeRequests().antMatchers("/song/genres").permitAll();
+        http.authorizeRequests().antMatchers("/song/status").permitAll();
+        http.authorizeRequests().antMatchers("/v2/api-docs").permitAll();
+        http.authorizeRequests().antMatchers("/swagger-resources/**").permitAll();
+        http.authorizeRequests().antMatchers("/swagger-ui/**").permitAll();
+        http.authorizeRequests().antMatchers("/webjars/**").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter());
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
